597614ad0fa1348029fc1489dd0b45383a1ed66b
991627aishu <nsnaishwaryabtech24@rvu.edu.in>
2024-11-04 17:11:10 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,11 +1,11 @@
-
+import streamlit as st
 import plotly.express as px
 import seaborn as sns
 import pandas as pd
 tips = sns.load_dataset("tips")
 fig1 = px.bar(tips,x="day",y="tip")
 fig2 = px.scatter(tips,x="total_bill",y="tip")
-st.tittle("data visualization with plotly")
+st.title("data visualization with plotly")
 st.header("Plot1:bar plot-Tips by Day")
 st.plotly_chart(fig1)
 st.markdown('''**Insight observed**:(Add your insights here)''')
